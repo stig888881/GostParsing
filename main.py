e1242ae3683de3d10d5ce45fefdaa382f006eac4
stig888881 <71179046+stig888881@users.noreply.github.com>
2022-05-29 21:54:02 +0300

    29/05/22

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ gost=soup.find_all('table', {'id': 'main'})
 for gost in gost:
     gost_title=gost.get('title')
     #aprove=gost.text
-    print(gost.text)
+    #print(gost.text)
     aprove.append(gost.text)
 result4=re.findall(r"ГОСТ Р \d{5}-\d{4}",aprove[0])
 result2=re.findall(r"ГОСТ Р \d{5}-\d{2}",aprove[0])
@@ -25,27 +25,40 @@ for i in range(len(result2)):
     m.append(g[1])
 a=np.array(m)
 b=a.reshape(-1,2)
-print('done')
-print(result4)
-print(result2)
+c=np.array(b).tolist()
+#print('done')
+#print(result4)
+#print(result2)
 
 import sqlite3
 
 conn = sqlite3.connect("sqlite.db")  # или :memory: чтобы сохранить в RAM
 cursor = conn.cursor()
-lens=len(result2)
-text_gost=['ГОСТ 8.1-2000','ГОСТ 8.2-2003']
-#cursor.execute("""CREATE TABLE tablegost
-#                  (gost text)
-#               """)
-
-for i in range(lens):
-    resgost=result2[i]
-    cursor.execute("INSERT INTO tablegost VALUES (?)", [resgost])
-    conn.commit()
-sql = "SELECT * FROM tablegost"
-cursor.execute(sql)
+#cursor.execute("DROP TABLE tablegost2")
+# cursor.execute("""CREATE TABLE tablegost2
+#                   (gost text)
+#                """)
+#
+# cursor.execute("""CREATE TABLE tablegost4
+#                   (gost text)
+#                """)
+#
+# for i in range(len(result2)):
+#     resgost=result2[i]
+#     cursor.execute("INSERT INTO tablegost2 VALUES (?)", [resgost])
+#     conn.commit()
+# cursor.execute("""CREATE TABLE tablegost2
+#                   (gost text)
+#                """)
+#
+# for i in range(len(result4)):
+#     resgost=result4[i]
+#     cursor.execute("INSERT INTO tablegost4 VALUES (?)", [resgost])
+#     conn.commit()
+#sql = "SELECT * FROM tablegost"
+p=[]
+conn.row_factory = lambda cursor, row: row[0]
+c = conn.cursor()
+p = c.execute('SELECT * FROM tablegost2').fetchall()
 print(cursor.fetchall())
 print('done')
-fat=cursor.fetchone()
-print('done2')
